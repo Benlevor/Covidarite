d279d21f006f54c03b8c2bec8440d75fe7face91
Benedict Agblevor <benlevor@gmail.com>
2021-08-17 10:40:12 +0200

    creation projet et bdd

---
+++ src/Controller/CovidController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Annonce;
+use App\Repository\AnnonceRepository;
+
+class CovidController extends AbstractController
+{
+    /**
+     * @Route("/covid", name="covid")
+     */
+    public function index(AnnonceRepository $repo): Response
+    {
+        $annonces = $repo->findAll();
+
+        return $this->render('covid/index.html.twig', [
+            'controller_name' => 'CovidController',
+            'annonces' => $annonces
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(){
+        return $this ->render('covid/home.html.twig', [
+            'title' => "Bienvenue sur le site de solidarité contre le Covid"
+        ]);
+    }
+
+    /**
+     * @Route("/covid/{id}", name="covid_show")
+     */
+    public function show(Annonce $annonce){
+
+        return $this->render('covid/show.html.twig',[
+            'annonce'=> $annonce
+        ]);
+    }
+}
