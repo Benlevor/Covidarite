298156c0af4b179e4ca7d5a58edf4b9d0b11bad3
Benedict Agblevor <benlevor@gmail.com>
2021-08-18 06:50:11 +0200

    Forms avec attr dans controller

--- src/Controller/CovidController.php
+++ src/Controller/CovidController.php
@@ -7,6 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Annonce;
 use App\Repository\AnnonceRepository;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CovidController extends AbstractController
 {
@@ -36,9 +39,37 @@ class CovidController extends AbstractController
      * @Route("/covid/new", name="covid_create")
      */
     public function create() {
-        return $this->render('covid/create.html.twig');
+        $annonce= new Annonce();
+
+        $form= $this->createFormBuilder($annonce)
+                    ->add('title', TextType::class,[
+                        'attr'=>[
+                            'placeholder'=> "Titre de l'annonce",
+                        ]
+                    ])
+                    ->add('content',TextareaType::class,[
+                        'attr'=>[
+                            'placeholder'=>"Veuillez saisir le contenu de votre annonce"
+                        ]
+                    ])
+                    ->add('image',TextType::class,[
+                        'attr'=>[
+                            'placeholder'=> "URL de l'image"
+                        ]
+                    ])
+
+                    ->add('type',TextType::class,[
+                        'attr'=>[
+                            'placeholder'=> "Type d'utilisateur"
+                        ]
+                    ])
+                    ->getForm();
+
+        return $this->render('covid/create.html.twig', [
+            'formAnnonce'=> $form->createView()
+        ]);
     }
-    
+
     /**
      * @Route("/covid/{id}", name="covid_show")
      */
